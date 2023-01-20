ad83fca78d87d77589f46be39fee1ea232e781cf
rameshwarlamak2305 <102512105+rameshwarlamak2305@users.noreply.github.com>
2023-01-20 23:04:39 +0530

    Add files via upload

---
+++ PRACTICAL/Java1.java
@@ -0,0 +1,12 @@
+class Anudip {
+	public static void display(){
+		System.out.println("Thank You Mam");
+	}
+	}
+	class Java1{
+		public static void main(String[] args){
+
+		  System.out.println("We are intered in Java programming World");
+		  Anudip.display();
+	}
+}
