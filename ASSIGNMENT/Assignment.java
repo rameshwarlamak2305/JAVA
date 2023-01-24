472bc343a3272c60ad33098987d171656961e484
rameshwarlamak2305 <102512105+rameshwarlamak2305@users.noreply.github.com>
2023-01-24 21:56:46 +0530

    Add files via upload

---
+++ ASSIGNMENT/Assignment.java
@@ -0,0 +1,16 @@
+public class Assignment 
+{ 
+
+ public static void main(String[] args) 
+ { 
+   int x , y = 30, z = 50; 
+    x=y; 
+    y= x; 
+    z= x * y;
+
+ System.out.println("x = " +x ); 
+ System.out.println("y = " +y ); 
+ System.out.println("z = " +z ); 
+
+ } 
+}
