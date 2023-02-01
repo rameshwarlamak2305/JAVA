547d61925930831a8ef12e52658ff74e7fb5ba84
rameshwarlamak2305 <102512105+rameshwarlamak2305@users.noreply.github.com>
2023-02-01 23:15:48 +0530

    Add files via upload

---
+++ PRACTICAL/LoopInJava.java
@@ -0,0 +1,37 @@
+
+class LoopInJava {
+	public static void main(String[] args){
+		// for loop example
+		int i;
+		for ( i = 1; i <= 10; i++) {
+      System.out.println("Ram");
+		}
+
+	// while loop example
+     i = 1;
+    while (i < 5) {
+      System.out.println("Learning Java at Anudip");
+      System.out.println("The value of i is = " + i);
+      i++;
+    }
+    System.out.println("end of while");
+  
+
+  
+  // do while loop example
+    i = 1;
+    do {
+      
+      System.out.println(" Java ocean ");
+      i++;
+
+    }
+    while ( i != 5 );
+  
+
+}
+
+}
+	
+
+	
