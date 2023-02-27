f8b65f359ec0154eda2de208b4c5bc109847e443
rameshwarlamak2305 <102512105+rameshwarlamak2305@users.noreply.github.com>
2023-02-27 18:40:12 +0530

    Add files via upload

---
+++ LAB/Area.java
@@ -0,0 +1,11 @@
+package shapes;
+
+public class Area {//defining class area 
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		Circle c=new Circle(8);//constructor invoked here 
+
+	}
+
+}
