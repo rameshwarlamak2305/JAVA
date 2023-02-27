1745119671d8663bda22ef6ea87371d923b397f6
rameshwarlamak2305 <102512105+rameshwarlamak2305@users.noreply.github.com>
2023-02-27 18:39:35 +0530

    Add files via upload

---
+++ LAB/Shape.java
@@ -0,0 +1,12 @@
+package geometry;
+
+import shapes.Circle; // to invoke the circle class in this Shape class we have to import this .
+
+public class Shape {//defining the shape class 
+
+	public static void main(String[] args) { 
+		@SuppressWarnings("unused")
+		Circle c = new Circle(); // by creating the object of circle class with the defualt constructor Circle()
+
+	}
+}
