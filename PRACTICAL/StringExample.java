0475068f1c7fab3f9a7f66ef709c623b8577b8d8
rameshwarlamak2305 <102512105+rameshwarlamak2305@users.noreply.github.com>
2023-03-01 21:59:59 +0530

    Add files via upload

--- PRACTICAL/StringExample.java
+++ PRACTICAL/StringExample.java
@@ -1,37 +1,35 @@
-package practical;
+package test;
 
-public class StringExample {
-	public static void main(String[] args) {	
-		//----------Comparison---------
-		String a1 = "Anudip";
-		String a4= "Anudip";
-		String a3= new String("anudip");
-		String a2= "Anudip Foundation Vashi";
-		//equals() compares values of String for equality
-		System.out.println(a1.equals(a2));  //false
-		System.out.println(a1.equals(a3)); //false, coz case mismatch
-	System.out.println(a1.equalsIgnoreCase(a3));  //true
-		
-	//== compares references not values
-	System.out.println(a1==a2); //false
-	System.out.println(a1==a4); //true
-		
-	//lexicographically, compareTo()  
-	String a5 = "apple";
-	String a6= "america";
-	System.out.println(a6.compareTo(a5)); //negative
-	System.out.println(a1.compareTo(a4)); //zero
-	System.out.println(a2.compareTo(a1)); //positive
-		
-	//------concatenation-------
-	System.out.println(a5+ " " +a6);
-	System.out.println(60+70+"my"+90+7.4);//before String its +, after string '+' as concat
-	System.out.println(a5.concat(a6));
+class student{//defines a class student
+	//declaring 2 instance variables with no value assigned
+	int id;
+	String name;
 	
-	
-	//------substring-----
-	 System.out.println(a2.substring(0, 2));
-	 System.out.println(a2.substring(6));
-	 System.out.println(a2.substring(17));
+//	creating a parameterized constructor which sets value to instance variables
+	public student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
 	}
+//	overriding  the method toString() to return value of both instance variable
+	@Override
+	public String toString() {
+		return id +" "+name;
+	}
+	
+}
+
+
+public class StringExample {// class with main()
+
+
+	public static void main(String[] args) {
+//		creating objects of class students 
+		student s1= new student(1, "Ram");
+		student s2= new student(2, "sam");
+//		printing the objects with sopln
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+
 }
