56eac2496315544c89dca959447bf9260ca0dc13
rameshwarlamak2305 <102512105+rameshwarlamak2305@users.noreply.github.com>
2023-03-09 17:52:54 +0530

    Add files via upload

---
+++ PRACTICAL/VerificationException.java
@@ -0,0 +1,15 @@
+package test;
+
+public class VerificationException extends Exception{
+	
+	private static final long serialversionUID = 1L;
+	public VerificationException(String str) {
+		
+		
+		//private static final long serialversionUID = 1L;
+		 
+			super(str);
+			}
+
+	
+}
